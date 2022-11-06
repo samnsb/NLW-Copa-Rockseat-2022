1a7fb9d463fafe01900334a8134aeaef119ae6cd
Samela <samelnsb@gmail.com>
2022-11-06 12:24:18 -0300

    NLW Copa - Jogos do Brasil

--- main.js
+++ main.js
@@ -22,7 +22,6 @@ function createCard(date, day, games) {
 document.querySelector("#cards").innerHTML =
   createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
   createCard("28/11", "segunda",
-    createGame("korea", "10:00", "ghana") + 
     createGame("brazil", "13:00", "switzerland")
     ) +
   createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
